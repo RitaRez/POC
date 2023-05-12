25c6e8953fa26f74f32fa4c4bdc721b77f8ad847
RitaRez <ritarezborges@gmail.com>
2023-05-12 18:11:43 -0300

    code is so much better

--- dense_retrieval/model_fine_tuning.py
+++ dense_retrieval/model_fine_tuning.py
@@ -1,93 +1,86 @@
 import json, torch, time, torch
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
 from sentence_transformers import SentenceTransformer
 from torch.utils.data import DataLoader
 
 
-
-def read_corpus(corpus_path: str) -> dict[str]:
-    """
-    Read the corpus from file_path
-    """
-    
-    films_data = {}
-    with open(corpus_path, 'r') as f:
-        for line in f:    
-            film = json.loads(line)            
-            films_data[film['id']] = film['title'] + ' ' + film['text'] #+ ' ' + film['genres'] + ' ' + film['startYear']
-
-    return films_data
-
-
-def read_queries(queries_path: str) -> dict[str]:
-    """
-    Read the queries from file_path
-    """
-
-    queries = {}
-    with open(queries_path, 'r') as f:
-        for line in f:    
-            query = json.loads(line)            
-            queries[query['id']] = query['title'] + ' ' + query['description']
-
-    return queries
-
-
-def split_qrels_train_test(qrels_path: str) -> tuple[pd.Dataframe, pd.Dataframe]:
-    """
-    Split the corpus into train and test
-    """
-    
-    df = pd.read_csv(qrels_path, sep="\s+", names=["query_id", "_", "movie_id", "label"]) # For header names
-    df.drop('_', axis=1, inplace=True)
-
-    train, test = train_test_split(df, test_size=0.2)
-
-    return train, test
-
-
-def prepare_train_set(corpus: dict, queries: dict, qrels_path: str) -> list[InputExample]:
+def prepare_train_set(corpus: dict, train: pd.DataFrame, queries: dict, qrels_path: str) -> list[InputExample]:
     """
     Prepare the train set
     """
 
-    train, _  = split_qrels_train_test(qrels_path)
-
-    train_data = []
+    count = 0
+    train_data = []; test_data = []
+    
     for index, row in train.iterrows():  
-        query_id = row['query_id']; movie_id = row['movie_id']; label = row['label']
-        train_data.append(InputExample(texts=[queries[query_id], corpus[movie_id]], label=label))
+        query_id = row['query_id']; movie_id = row['movie_id']; label = float(row['label'])
+        if query_id not in queries or movie_id not in corpus:
+            count += 1
+            continue
+        else:
+            query = queries[query_id]
+            film_text = corpus[movie_id]
+            train_data.append(InputExample(texts=[query, film_text], label=label))
+    
 
+    print(f"Had to skip {count} pairs")
+    
     return train_data
 
 
-def train_model(corpus: dict, queries: dict, model_path: str):
+def train_model(train_examples: list[InputExample], model_path: str, qrels_path: str):
     """
     Train the model
     """
 
-    model = SentenceTransformer('bert-base-uncased')
+    model = SentenceTransformer('all-MiniLM-L6-v2', device='cuda')
 
-    train_examples = prepare_train_set(corpus, queries)
-    
-    train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=16)
+    train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=64)
     train_loss = losses.CosineSimilarityLoss(model)
-    
-    model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=1, warmup_steps=100)
-    model.save(model_path)
 
+    model.fit(train_objectives=[(train_dataloader, train_loss)],
+          epochs = 2,
+          evaluation_steps = 1000,
+          warmup_steps = math.ceil(len(train_dataloader) * 4 * 0.1))
+    
+    model.save(model_path)
+    
     return model
 
 
-def encode_corpus(corpus: dict, embedings_path: str, model_path: str):
+def encode_test_set(test: pd.DataFrame, test_path: str):
+    """
+    With the finetuned model we will encode the test set and store it
+    """
+
+    test_data = []
+    for index, row in test.iterrows():  
+        query_id = row['query_id']; movie_id = row['movie_id']; label = float(row['label'])
+        if query_id not in queries or movie_id not in corpus:
+            count += 1
+            continue
+        else:
+            query = queries[query_id]
+            test_data.append(query)
+
+    pool = model.start_multi_process_pool()
+    test_embeddings = model.encode_multi_process(test_data, pool)
+
+    with open(test_path, "wb") as fOut:
+        pickle.dump({'sentences': test_data, 'embeddings': test_embeddings}, fOut, protocol=pickle.HIGHEST_PROTOCOL)
+
+    return test_embeddings    
+
+
+def encode_corpus(corpus: dict, model_path: str, embedings_path: str):
     """
     Encode the corpus using SentenceTransformer
     """
 
     model = SentenceTransformer(model_path)
-    embeddings = model.encode(corpus[:10])
+    pool = model.start_multi_process_pool()
+    embeddings = model.encode_multi_process(corpus.values(), pool)
 
     with open(embedings_path, "wb") as fOut:
         pickle.dump({'sentences': corpus, 'embeddings': embeddings}, fOut, protocol=pickle.HIGHEST_PROTOCOL)
@@ -96,31 +89,14 @@ def encode_corpus(corpus: dict, embedings_path: str, model_path: str):
     return embeddings
 
 
-if __name__ == "__main__":
+def load_embeddings(embedings_path: str):
+    """
+    Load pre trained embeddings
+    """
+    
+    with open(embedings_path, 'rb') as pkl:
+        data = pickle.load(pkl)
+        sentences = data['sentences']
+        embeddings = data['embeddings']
 
-    if not os.isfile('embeddings.pkl'):
-        
-        corpus = read_corpus("../Movies/documents.json")
-        queries = read_queries("../Movies/queries.json")
-        
-        if not os.isfile('../Models'):            
-            train_model(corpus, queries)
-        
-        else:    
-            embeddings = encode_corpus(corpus) 
-
-    else:
-
-        with open('embeddings.pkl', "rb") as fIn:
-            stored_data = pickle.load(fIn)
-            stored_sentences = stored_data['sentences']
-            embeddings = stored_data['embeddings']
-
-
-
-
-
-
-
-
-
+    return embeddings, sentences
